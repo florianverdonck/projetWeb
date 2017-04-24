193ca6a2633acc15896f65d86c03dd9971162fce
twizyx <info@twizyx.com>
2017-04-24 15:04:39 +0200

    Change views Student + login

--- views/login.php
+++ views/login.php
@@ -1,22 +1,11 @@
+<?php $page="login"; ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Page de connexion à la plateforme de prise de présences de l'IPL">
-		<meta name="author" content="Christopher CASTEL et Florian VERDONCK">
-		<title>IPL Présences - Page de connexion</title>
-		<!-- Bootstrap core CSS -->
-		<link href="<?=PATH_BS_CSS; ?>bootstrap.min.css" rel="stylesheet">
-		<!-- Custom CSS -->
-		<link href="<?=PATH_CSS; ?>custom.css" rel="stylesheet">
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-		<link href="<?=PATH_BS_CSS; ?>ie10-viewport.min.css" rel="stylesheet">
-	</head>
+  	<?php include_once(PATH_VIEWS . "header.php"); ?>
+  
 	<body>
+		<?php include_once(PATH_VIEWS . "navbar.php"); ?>
 		<div class="container">
-			
 			<div class="row pt50">
 				<div class="col-md-4 col-md-offset-4">
 					<div class="panel panel-primary">
@@ -40,7 +29,8 @@
 			</div>
 		</div>
 		<!-- /container -->
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-		<script src="<?=PATH_BS_JS; ?>ie10-viewport.min.js"></script>
+		
+		<?php include_once(PATH_VIEWS . "js_files.php"); ?>
+		
 	</body>
 </html>
---
+++ views/student.php
@@ -0,0 +1,205 @@
+<?php $page="student"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<?php include_once(PATH_VIEWS . "header.php"); ?>
+	<body>
+		<?php include_once(PATH_VIEWS . "navbar.php"); ?>
+		<div class="container">
+			<div class="row pt30">
+				<div class="col-md-4">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Filtrez ici pour voir vos présences</h3>
+						</div>
+						<div class="panel-body">
+							<form class="" action="index.php?page=student" method="post">
+								<label for="inputWeekSelect">Sélectionnez la semaine </label><br>
+								<select class="selectpicker" id="inputWeekSelect">
+									<option>Semaine n°1</option>
+									<option>Semaine n°2</option>
+									<option>Semaine n°3</option>
+									<option>Semaine n°4</option>
+									<option>Semaine n°5</option>
+									<option>Semaine n°6</option>
+									<option>Semaine n°7</option>
+									<option>Semaine n°8</option>
+									<option>Semaine n°9</option>
+								</select>
+								<br><br>
+								<label for="seanceTemplateSelect">Sélectionnez la séance type </label><br>
+								<select class="selectpicker" id="seanceTemplateSelect">
+									<option>Algo I</option>
+									<option>Algo II</option>
+									<option>Math EX I</option>
+								</select>
+								<br><br>
+								<label for="typeOfPresenceSelect">Sélectionnez le type de présence </label><br>
+								<select class="selectpicker" id="typeOfPresenceSelect">
+									<option>Présent</option>
+									<option>Absent</option>
+									<option>Certificat</option>
+								</select>
+								<br><br>
+								<button class="btn btn-lg btn-primary btn-block" type="submit">Filtrer</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Table de présences</h3>
+						</div>
+						<table class="table">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Matricule</th>
+									<th>Prénom</th>
+									<th>Nom de famille</th>
+									<th>Présence</th>
+									<th>Certificat</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">2</th>
+									<td>281</td>
+									<td>Christopher</td>
+									<td>Castel</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default active btn-sm">
+										    <input type="radio" name="options" id="option1" autocomplete="off"> Présent
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off" checked> Absent
+										  </label>
+										</div>
+									</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default btn-sm">
+										    <input type="checkbox" autocomplete="off"><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Justifié
+										  </label>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>121</td>
+									<td>Marco</td>
+									<td>Amory</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default active btn-sm">
+										    <input type="radio" name="options" id="option1" autocomplete="off"> Présent
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off" checked> Absent
+										  </label>
+										</div>
+									</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default btn-sm">
+										    <input type="checkbox" autocomplete="off"><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Justifié
+										  </label>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row">4</th>
+									<td>200</td>
+									<td>Florian</td>
+									<td>Verdonck</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default active btn-sm">
+										    <input type="radio" name="options" id="option1" autocomplete="off"> 0
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 1
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 2
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 3
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 4
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 5
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off" checked> Absent
+										  </label>
+										</div>
+									</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default btn-sm">
+										    <input type="checkbox" autocomplete="off"><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Justifié
+										  </label>
+										</div>
+									</td>								</tr>
+								<tr>
+									<th scope="row">4</th>
+									<td>200</td>
+									<td>Florian</td>
+									<td>Verdonck</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default active btn-sm">
+										    <input type="radio" name="options" id="option1" autocomplete="off"> 0
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 1
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 2
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 3
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 4
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off"> 5
+										  </label>
+										  <label class="btn btn-default btn-sm">
+										    <input type="radio" name="options" id="option2" autocomplete="off" checked> Absent
+										  </label>
+										</div>
+									</td>
+									<td>
+										<div class="btn-group" data-toggle="buttons">
+										  <label class="btn btn-default btn-sm">
+										    <input type="checkbox" autocomplete="off"><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Justifié
+										  </label>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div class="panel-body">
+							<label for="basic-url">Entrez un élève absent de la liste</label>
+							<div class="input-group">
+							  <span class="input-group-addon" id="basic-addon3">Matricule</span>
+							  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+							  <div class="input-group-btn">
+							    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Ajouter à la liste</button>
+							  </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /container -->
+		<?php include_once(PATH_VIEWS . "js_files.php"); ?>
+	</body>
+</html>
