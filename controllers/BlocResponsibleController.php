982fc2da01d1d2d9c3083dd531db5996aaa924d8
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-26 21:21:16 +0200

    Creation of bloc(s)Controller

---
+++ controllers/BlocResponsibleController.php
@@ -0,0 +1,14 @@
+<?php
+
+class BlocResponsibleController {
+	private $_db;
+	public function __construct($db) {
+		$this->_db = $db;
+	}
+		
+	public function run(){
+
+
+	}
+
+}
---
+++ controllers/BlocsResponsibleController.php
@@ -0,0 +1,14 @@
+<?php
+
+class BlocsResponsibleController {
+	private $_db;
+	public function __construct($db) {
+		$this->_db = $db;
+	}
+
+	public function run(){
+
+
+	}
+
+}
