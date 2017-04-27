427ecbd99c0fea7c34dc816985432fe6dacdd6c1
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-27 11:35:46 +0200

    form agenda function extracted

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -9,16 +9,23 @@ class AdminController {
 	public function run(){	
 		$update_message = '';
 		if (! empty ( $_POST ['form_agenda'] )) {
-			$update_message = 'ok 1 ';
-			if (! empty ( $_FILES ['userfile'] ['name'] )) {
-				$update_message = 'ok 2';
-				if (! preg_match ( '/\.agenda/', $_FILES ['userfile'] ['name'] )) {
-					$update_message =  'Le format est invalide.';
-				}
-			}
+			$update_message = $this->formAgenda ();
 		}
 		require_once (PATH_VIEWS . 'admin.php');
 		
 	}
+
+	private function formAgenda() {
+		if (! empty ( $_FILES ['userfile'] ['name'] )) {
+			if (! preg_match ( '/\.agenda/', $_FILES ['userfile'] ['name'] )) {
+				return 'Le format est invalide.';
+			}
+			$origine = $_FILES ['userfile'] ['tmp_name'];
+			$destination = 'conf/agenda.properties';
+			move_uploaded_file ( $origine, $destination );
+			return 'ok';
+		}
+	}
+
 	
 }
