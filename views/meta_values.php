84073b53d57c93be1b86dba8594b0236e6ae81cc
twizyx <info@twizyx.com>
2017-04-27 11:52:04 +0200

    Updated meta_values for new pages

--- views/meta_values.php
+++ views/meta_values.php
@@ -7,5 +7,17 @@ $metas = array(
 			"student" => array(
 					"title" => "IPL Présences - Etudiant",
 					"description" => "Voir ses présences étudiant"
+			),
+			"bloc_responsible" => array(
+					"title" => "IPL Présences - Gestionnaire de bloc",
+					"description" => "Gestionnaire de bloc : édition de série, UE et séances types"
+			),
+			"bloc_responsible_series" => array(
+					"title" => "IPL Présences - Edition des séries",
+					"description" => "Gestionnaire de bloc : édition des séries"
+			),
+			"bloc_responsible_seance_templates" => array(
+					"title" => "IPL Présences - Edition des séances types",
+					"description" => "Gestionnaire de bloc : édition des séances types"
 			)
 		);
