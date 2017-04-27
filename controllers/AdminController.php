5a1ef7266b14724e393ec9ba50bdff731b6882ec
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-27 11:32:07 +0200

    check if file is .agenda

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,7 +7,16 @@ class AdminController {
 	}
 			
 	public function run(){	
-
+		$update_message = '';
+		if (! empty ( $_POST ['form_agenda'] )) {
+			$update_message = 'ok 1 ';
+			if (! empty ( $_FILES ['userfile'] ['name'] )) {
+				$update_message = 'ok 2';
+				if (! preg_match ( '/\.agenda/', $_FILES ['userfile'] ['name'] )) {
+					$update_message =  'Le format est invalide.';
+				}
+			}
+		}
 		require_once (PATH_VIEWS . 'admin.php');
 		
 	}
