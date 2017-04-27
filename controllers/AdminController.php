7680fa478a63ead7d9eb09eb59fddebe89644967
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-27 16:26:20 +0200

    formProfessors function

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -11,6 +11,9 @@ class AdminController {
 		if (! empty ( $_POST ['form_agenda'] )) {
 			$update_message = $this->formAgenda ();
 		}
+		if (! empty ($_POST['form_professors'])) {
+			$update_message = $this->formProfessors ();
+		}
 		require_once (PATH_VIEWS . 'admin.php');
 		
 	}
@@ -29,7 +32,7 @@ class AdminController {
 			move_uploaded_file ( $origine, $destination );
 			$fcontents = file ( $destination );
 			// inserts each weeks in the db
-			foreach ( $fcontents as $i => $icontent ) {
+			foreach ( $fcontents as $icontent ) {
 				preg_match ( '/^(.*)_(.*)=(.*)$/', $icontent, $result );
 				if (!$this->_db->insert_week($result[2],$result[1],$result[3])){
 					$update_message['error_code'] = 'danger';
@@ -42,4 +45,32 @@ class AdminController {
 			return $update_message;
 		}
 	}	
+	private function formProfessors() {
+		if (! empty ( $_FILES ['userfile'] ['name'] )) {
+			// checks if file extension is csv
+			if (! preg_match ( '/\.csv/', $_FILES ['userfile'] ['name'] )) {
+				$update_message['error_code'] = 'danger';
+				$update_message['error_message'] = 'Le fichier doit avoir une extension .csv';
+				return $update_message;
+			}
+			// saves professors file to conf directory
+			$origine = $_FILES ['userfile'] ['tmp_name'];
+			$destination = 'conf/professeurs.csv';
+			move_uploaded_file ( $origine, $destination );
+			$fcontents = file ( $destination );
+			// inserts each professor in the db
+			$test = 0;
+			foreach ( $fcontents as $icontent ) {
+				if ($test != 0) {
+					preg_match ( '/^(.*);(.*);(.*);(.*)$/', $icontent + 1, $result );
+					if (!empty($result))
+						$this->_db->insert_professor($result[1],$result[2],$result[3],$result[4]);
+				}
+				$test++;
+			}
+			$update_message['error_code'] = 'success';
+			$update_message['error_message'] = 'Les professeurs ont été ajoutés avec succès';
+			return $update_message;
+		}
+	}
 }
