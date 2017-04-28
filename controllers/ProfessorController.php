22a2339baa81f150c81524815fdc94fbc3b6c1e0
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-28 11:32:05 +0200

    insert scripts

--- controllers/ProfessorController.php
+++ controllers/ProfessorController.php
@@ -7,7 +7,15 @@ class ProfessorController {
 	}
 			
 	public function run(){	
-	
+		
+//		$this->_db->insert_week(13, 2, '11/05/2017);
+//		$this->_db->insert_professor('mail@mail', 'test', 'test', 'true');
+// 		$this->_db->insert_course('test', 'test', 1, 2, 3, 't', 'UE');
+// 		$this->_db->insert_serie(2, 'serie 1 bloc 2');
+// 		$this->_db->insert_student('mail@mail.mail', 1, 'Test', 'Test');
+// 		$this->_db->insert_seance_template('algo1', 'Notée', 'test');
+// 		$this->_db->insert_seance_template('algo2', 'XO', 'test');
+
 		require_once(PATH_VIEWS . "professor.php");
 	}
 }
