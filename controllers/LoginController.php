9e7adc564bd16a0748507797d7e95569d5ccd8ed
ChristopherCastel <castelchristopher95@gmail.com>
2017-04-30 12:30:26 +0200

    Login controller with roles bases access control

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,19 +1,105 @@
 <?php
-	 
+
 class LoginController {
 	private $_db;
 	public function __construct($db) {
 		$this->_db = $db;
 	}
-			
-	public function run(){	
-
-		if (isset($_POST['inputEmail'])) {
-			print_r($_POST);
-		}
-
-		require_once(PATH_VIEWS . "login.php");
 		
+	public function run(){
+		// checks if the user is already logged in
+		if (! empty ( $_SESSION ['authenticated'] )) {
+			$this->checkPermissions ();
+		}
+		$update_message = '';
+		if (! empty ( $_POST ['form_login'] )) {
+			$update_message = $this->formLogin ();
+		}
+		require_once(PATH_VIEWS . "login.php");
+	}
+
+	// redirects according to permissions
+	private function checkPermissions() {
+		$responsible = $_SESSION ['authenticated'];
+		switch ($responsible) {
+			case "admin":
+				header ( 'Location: index.php?user=admin' );
+				die ();
+				break;
+			case "student":
+				header ( 'Location: index.php?user=student' );
+				die ();
+				break;
+			case "professor":
+				header ( 'Location: index.php?user=professor' );
+				die ();
+				break;
+			case "bloc_responsible":
+				header ( 'Location: index.php?user=bloc_responsible' );
+				die ();
+				break;
+			case "blocs_responsible":
+				header ( 'Location: index.php?user=blocs_responsible' );
+				die();
+		}
+	}
+
+	private function formLogin() {
+		$mail = $_POST ['inputEmail'];
+		// checks if input is empty
+		if ($mail == null ) {
+			return $this->connectionErrorMessage ( );
+		}
+		if (preg_match('/@student\.vinci\.be$/', $mail)) { // is a student
+			$student = $this->_db->select_student($mail);
+			if (empty($student)) { // student doesn't exist
+				return $this->connectionErrorMessage ( );
+			}
+			$_SESSION ['authenticated'] = 'student';
+			$_SESSION['user'] = serialize($student);
+			header ( 'Location: index.php?action=student' );
+			die ();
+		}
+		if (preg_match('/@vinci\.be$/', $mail)) { // is a professor
+			$professor = $this->_db->select_professor($mail);
+			if (empty($professor)) { // professor doesn't exist
+				return $this->connectionErrorMessage ( );
+			}
+			$this->setAuthenficationProfessor ($professor);
+		}
+		return $this->connectionErrorMessage ();
 	}
 	
+	private function setAuthenficationProfessor($professor){
+		$responsible = $professor->responsible();
+		$_SESSION['user'] = serialize($professor);
+		switch ($responsible) {
+			case true:
+				$_SESSION ['authenticated'] = 'admin';
+				header ( 'Location: index.php?user=admin' );
+				die ();
+				break;
+			case false:
+				$_SESSION ['authenticated'] = 'professor';
+				header ( 'Location: index.php?user=professor' );
+				die ();
+				break;
+			case "blocs":
+				$_SESSION ['authenticated'] = 'blocs_responsible';
+				header ( 'Location: index.php?user=blocs_responsible' );
+				die ();
+				break;
+			default:
+				$_SESSION ['authenticated'] = 'bloc_responsible';	
+				header ( 'Location: index.php?user=bloc_responsible' );
+				die ();
+		}
+	}
+
+	private function connectionErrorMessage () {
+		$update_message['error_code'] = 'danger';
+		$update_message['error_message'] = 'La connexion a échoué. Veuillez vérifier l\'adresse mail.';
+		return $update_message;
+	}
+
 }
