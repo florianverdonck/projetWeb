60ff274298c93f0fa2f0457ee11b8ae3875b440d
ChristopherCastel <castelchristopher95@gmail.com>
2017-05-04 12:38:35 +0200

    seance template class corrected

--- models/Seance_template.class.php
+++ models/Seance_template.class.php
@@ -3,10 +3,9 @@ class Seance_template {
 	private $_name;
 	private $_attendance_type;
 	private $_responsible;
-	public function __construct($name, $attendance_type, $responsible) {
+	public function __construct($name, $attendance_type) {
 		$this->_name = $name;
 		$this->_attendance_type = $attendance_type;
-		$this->_responsible = $responsible;
 	}
 	public function name() {
 		return $this->_name;
