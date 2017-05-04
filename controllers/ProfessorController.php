45d3378541a197e5d30eb7115ec2c31659b14ff4
ChristopherCastel <castelchristopher95@gmail.com>
2017-05-04 19:41:53 +0200

    new functions for attendances professor controller

--- controllers/ProfessorController.php
+++ controllers/ProfessorController.php
@@ -10,34 +10,54 @@ class ProfessorController {
 			header ( 'Location: index.php?action=login' );
 			die ();
 		}
+		$sorted_attendances = false;
+		$update_message = '';
+		$students = '';
 		$weeks_created = $this->_db->existing_weeks ();
 		$seances_created = $this->_db->existing_seances ();
 		$seances_templates = '';
+		
+		// Loads a more in depth form based on previous in puts
 		if (isset ( $_POST ['bloc'] ) && isset ( $_POST ['term'] )) {
 			$seances_templates = $this->_db->select_seance_templates ( $_POST ['bloc'], $_POST ['term'] );
 			$series = $this->_db->select_series_from_bloc_term ( $_POST ['bloc'], $_POST ['term'] );
+			$sorted_attendances = true;
 		}
-		$update_message = '';
-		$students = '';
-		if (! empty ( $_POST ['form_attendance'] )) {
-			$update_message = $this->formAttendance ();
+		
+		// Sorts students by seance, serie, term and bloc  
+		if (! empty ( $_POST ['form_sort_attendances'] )) {
+			$update_message = $this->formSortAttendance ();
 			// everything went well, no error message
-			if (!isset($update_message)) {
-				$students = $this->_db->select_students_from_course($_POST ['seance'], $_POST['serie'], $_POST['term'], $_POST['bloc']);
+			if (! isset ( $update_message )) {
+				$students = $this->_db->select_students_from_course ( $_POST ['seance'], $_POST ['serie'], $_POST ['term'], $_POST ['bloc'] );
 			}
 		}
 		
-		// $this->_db->insert_week(13, 2, '11/05/2017);
-		// $this->_db->insert_professor('mail@mail', 'test', 'test', 'true');
-		// $this->_db->insert_course('test', 'test', 1, 2, 3, 't', 'UE');
-		// $this->_db->insert_serie(2, 1, 2);
-		// $this->_db->insert_student('mail@mail.mail', 1, 'Test', 'Test');
-		// $this->_db->insert_seance_template('algo1', 'Notée', 'test');
-		// $this->_db->insert_seance_template('algo2', 'XO', 'test');
+		// Takes attendances for listed students
+		if (! empty ( $_POST ['form_take_attendances'] )) {
+			$update_message = array (
+					"error_code" => "success",
+					"error_message" => "Les présences ont été prises." 
+			);
+		}
 		
-		require_once (PATH_VIEWS . "professor.php");
+		// Allows the professor to add a student which isn't from the current serie
+		if (!empty ($_POST['form_add_student_attendance'])) {
+			$update_message = array (
+					"error_code" => "success",
+					"error_message" => "L'élèves a été ajouté dans la liste des présences."
+			);
+		}
+		
+		if (! $sorted_attendances) {
+			require_once (PATH_VIEWS . "professor.php");
+		} else {
+			require_once (PATH_VIEWS . "professor.attendances_sorted.php");
+		}
 	}
-	private function formAttendance() {
+	
+	// Returns errors messages if form inputs are not correctly set
+	private function formSortAttendance() {
 		if (empty ( $_POST ['bloc'] )) {
 			return array (
 					"error_code" => "danger",
@@ -70,10 +90,10 @@ class ProfessorController {
 			);
 		}
 		
-		if (empty ($_POST ['attendance_type'])) {
+		if (empty ( $_POST ['attendance_type'] )) {
 			return array (
 					"error_code" => "danger",
-					"error_message" => "Veuillez sélectionner un type de présence."
+					"error_message" => "Veuillez sélectionner un type de présence." 
 			);
 		}
 	}
