091761cce944c8de54974f2710757702d556664e
ChristopherCastel <castelchristopher95@gmail.com>
2017-05-05 19:14:58 +0200

    student id added

--- models/Student.class.php
+++ models/Student.class.php
@@ -1,15 +1,19 @@
 <?php
 class Student {
+	private $_student_id;
 	private $_mail;
 	private $_name;
 	private $_first_name;
 	private $_bloc;
-	
-	public function __construct($mail, $name, $first_name, $bloc) {
-		$this->_mail 		= $mail;
-		$this->_name 		= $name;
-		$this->_first_name	= $first_name;
-		$this->_bloc 		= $bloc;
+	public function __construct($student_id, $mail, $name, $first_name, $bloc) {
+		$this->_student_id = $student_id;
+		$this->_mail =       $mail;
+		$this->_name =       $name;
+		$this->_first_name = $first_name;
+		$this->_bloc =       $bloc;
+	}
+	public function student_id() {
+		return $this->_student_id;
 	}
 	public function mail() {
 		return $this->_mail;
@@ -22,6 +26,9 @@ class Student {
 	}
 	public function bloc() {
 		return $this->_bloc;
+	}
+	public function html_student_id() {
+		return htmlspecialchars ( $this->_student_id );
 	}
 	public function html_mail() {
 		return htmlspecialchars ( $this->_mail );
